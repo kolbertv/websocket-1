48d82e03ac013e0e849157dc63907e6b2e80cc17
kolbert vladimir <kolbert@yandex.ru>
2019-03-26 16:58:40 +0300

    123

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const ws = require('ws');
+
+const server = new ws.Server({
+    port: 2999
+});
+
+server.on('connection', result => {
+    result.send(`["message:send", ${JSON.stringify({
+        id: 'fghjkl',
+        device_id: 1234,
+        message: 'ответ сервера'
+    })}]`);
+});
