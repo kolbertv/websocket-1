fbf15458408086a8ce9be746cdb94a8a4f620db6
kolbert vladimir <kolbert@yandex.ru>
2019-04-09 16:48:36 +0300

    123

--- app/app.js
+++ app/app.js
@@ -4,10 +4,18 @@ const server = new ws.Server({
     port: 2999
 });
 
+// server.on('connection', result => {
+//     result.send(`["message:send", ${JSON.stringify({
+//         id: 'fghjkl',
+//         device_id: 1234,
+//         message: 'ответ сервера'
+//     })}]`);
+// });
+
 server.on('connection', result => {
-    result.send(`["message:send", ${JSON.stringify({
+    result.send(JSON.stringify({
         id: 'fghjkl',
         device_id: 1234,
         message: 'ответ сервера'
-    })}]`);
+    }))
 });
